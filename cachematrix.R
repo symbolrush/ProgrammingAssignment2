22118e6c81958b783d20fdcdb796b48cf93a460f
Zardt <trompetenschmid@gmail.com>
2014-04-24 13:25:58 +0200

    Update cachematrix.R
    This file was created for the Coursera Course called "Programming in R".

--- cachematrix.R
+++ cachematrix.R
@@ -4,7 +4,17 @@
 ## makeCacheMatrix: This function creates a special 'matrix' object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(solve) m <<- solve
+        getmean <- function() m
+        list(set = set, get = get,
+             setmean = setmean,
+             getmean = getmean)
 }
 
 
